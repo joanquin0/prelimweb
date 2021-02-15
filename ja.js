e09a7e6267bf59ad67a8654e8f25f49847d1836d
joanquin0 <72197121+joanquin0@users.noreply.github.com>
2021-02-16 01:01:25 +0800

    java

--- ja.js
+++ ja.js
@@ -1,6 +1,6 @@
 let btn = document.querySelector('.product-1btn');
-let pbtn = document.querySelector('.button22');
-let mbtn = document.querySelector('.button2');    
+let pbtn = document.querySelector('.buttonn1');
+let mbtn = document.querySelector('.button1');    
 
 btn.addEventListener("click",()=>{
      btn.innerText = '1';
@@ -9,16 +9,395 @@ btn.addEventListener("click",()=>{
      
 })
 mbtn.addEventListener("click",()=>{
-     btn.innerText = btn.innerText - 1;
-     pbtn.style.display = 'inline-block';
-     mbtn.style.display = 'inline-block';
+ if(btn.innerText<2){
+      btn.innerText = 'ADD TO CART';
+      pbtn.style.display = 'none';
+      mbtn.style.display = 'none';
+     
+ }else{
+      btn.innerText = btn.innerText -1;
+ }
      
 })
 pbtn.addEventListener("click",()=>{
-     btn.innerText = btn.innerText + 1;
-     pbtn.style.display = 'inline-block';
-     mbtn.style.display = 'inline-block';
+     btn.innerText = +(btn.innerText) + 1;
+     if(btn.innerText == 5){
+          pbtn.style.display = 'none';
+          
+     }
      
 })
 
 
+
+
+{
+     let btn = document.querySelector('.product-2btn');
+     let pbtn = document.querySelector('.buttonn2');
+     let mbtn = document.querySelector('.button2');    
+     
+     btn.addEventListener("click",()=>{
+          btn.innerText = '1';
+          pbtn.style.display = 'inline-block';
+          mbtn.style.display = 'inline-block';
+          
+     })
+     mbtn.addEventListener("click",()=>{
+      if(btn.innerText<2){
+           btn.innerText = 'ADD TO CART';
+           pbtn.style.display = 'none';
+           mbtn.style.display = 'none';
+          
+      }else{
+           btn.innerText = btn.innerText -1;
+      }
+          
+     })
+     pbtn.addEventListener("click",()=>{
+          btn.innerText = +(btn.innerText) + 1;
+          if(btn.innerText == 5){
+               pbtn.style.display = 'none';
+               
+          }
+          
+     })
+     
+}
+
+
+{
+     let btn = document.querySelector('.product-3btn');
+     let pbtn = document.querySelector('.buttonn3');
+     let mbtn = document.querySelector('.button3');    
+     
+     btn.addEventListener("click",()=>{
+          btn.innerText = '1';
+          pbtn.style.display = 'inline-block';
+          mbtn.style.display = 'inline-block';
+          
+     })
+     mbtn.addEventListener("click",()=>{
+      if(btn.innerText<2){
+           btn.innerText = 'ADD TO CART';
+           pbtn.style.display = 'none';
+           mbtn.style.display = 'none';
+          
+      }else{
+           btn.innerText = btn.innerText -1;
+      }
+          
+     })
+     pbtn.addEventListener("click",()=>{
+          btn.innerText = +(btn.innerText) + 1;
+          if(btn.innerText == 5){
+               pbtn.style.display = 'none';
+               
+          }
+          
+     })
+     
+}
+
+{
+     let btn = document.querySelector('.product-4btn');
+     let pbtn = document.querySelector('.buttonn4');
+     let mbtn = document.querySelector('.button4');    
+     
+     btn.addEventListener("click",()=>{
+          btn.innerText = '1';
+          pbtn.style.display = 'inline-block';
+          mbtn.style.display = 'inline-block';
+          
+     })
+     mbtn.addEventListener("click",()=>{
+      if(btn.innerText<2){
+           btn.innerText = 'ADD TO CART';
+           pbtn.style.display = 'none';
+           mbtn.style.display = 'none';
+          
+      }else{
+           btn.innerText = btn.innerText -1;
+      }
+          
+     })
+     pbtn.addEventListener("click",()=>{
+          btn.innerText = +(btn.innerText) + 1;
+          if(btn.innerText == 5){
+               pbtn.style.display = 'none';
+               
+          }
+          
+     })
+     
+}
+
+{
+let btn = document.querySelector('.product-5btn');
+let pbtn = document.querySelector('.buttonn5');
+let mbtn = document.querySelector('.button5');    
+
+btn.addEventListener("click",()=>{
+     btn.innerText = '1';
+     pbtn.style.display = 'inline-block';
+     mbtn.style.display = 'inline-block';
+     
+})
+mbtn.addEventListener("click",()=>{
+ if(btn.innerText<2){
+      btn.innerText = 'ADD TO CART';
+      pbtn.style.display = 'none';
+      mbtn.style.display = 'none';
+     
+ }else{
+      btn.innerText = btn.innerText -1;
+ }
+     
+})
+pbtn.addEventListener("click",()=>{
+     btn.innerText = +(btn.innerText) + 1;
+     if(btn.innerText == 5){
+          pbtn.style.display = 'none';
+          
+     }
+     
+})
+
+}
+
+{
+     let btn = document.querySelector('.product-6btn');
+let pbtn = document.querySelector('.buttonn6');
+let mbtn = document.querySelector('.button6');    
+
+btn.addEventListener("click",()=>{
+     btn.innerText = '1';
+     pbtn.style.display = 'inline-block';
+     mbtn.style.display = 'inline-block';
+     
+})
+mbtn.addEventListener("click",()=>{
+ if(btn.innerText<2){
+      btn.innerText = 'ADD TO CART';
+      pbtn.style.display = 'none';
+      mbtn.style.display = 'none';
+     
+ }else{
+      btn.innerText = btn.innerText -1;
+ }
+     
+})
+pbtn.addEventListener("click",()=>{
+     btn.innerText = +(btn.innerText) + 1;
+     if(btn.innerText == 5){
+          pbtn.style.display = 'none';
+          
+     }
+     
+})
+
+}
+
+
+
+{
+let btn = document.querySelector('.product-7btn');
+let pbtn = document.querySelector('.buttonn7');
+let mbtn = document.querySelector('.button7');    
+
+btn.addEventListener("click",()=>{
+     btn.innerText = '1';
+     pbtn.style.display = 'inline-block';
+     mbtn.style.display = 'inline-block';
+     
+})
+mbtn.addEventListener("click",()=>{
+ if(btn.innerText<2){
+      btn.innerText = 'ADD TO CART';
+      pbtn.style.display = 'none';
+      mbtn.style.display = 'none';
+     
+ }else{
+      btn.innerText = btn.innerText -1;
+ }
+     
+})
+pbtn.addEventListener("click",()=>{
+     btn.innerText = +(btn.innerText) + 1;
+     if(btn.innerText == 5){
+          pbtn.style.display = 'none';
+          
+     }
+     
+})
+
+}
+
+
+{
+let btn = document.querySelector('.product-8btn');
+let pbtn = document.querySelector('.buttonn8');
+let mbtn = document.querySelector('.button8');    
+
+btn.addEventListener("click",()=>{
+     btn.innerText = '1';
+     pbtn.style.display = 'inline-block';
+     mbtn.style.display = 'inline-block';
+     
+})
+mbtn.addEventListener("click",()=>{
+ if(btn.innerText<2){
+      btn.innerText = 'ADD TO CART';
+      pbtn.style.display = 'none';
+      mbtn.style.display = 'none';
+     
+ }else{
+      btn.innerText = btn.innerText -1;
+ }
+     
+})
+pbtn.addEventListener("click",()=>{
+     btn.innerText = +(btn.innerText) + 1;
+     if(btn.innerText == 5){
+          pbtn.style.display = 'none';
+          
+     }
+     
+})
+
+}
+
+{
+     let btn = document.querySelector('.product-9btn');
+let pbtn = document.querySelector('.buttonn9');
+let mbtn = document.querySelector('.button9');    
+
+btn.addEventListener("click",()=>{
+     btn.innerText = '1';
+     pbtn.style.display = 'inline-block';
+     mbtn.style.display = 'inline-block';
+     
+})
+mbtn.addEventListener("click",()=>{
+ if(btn.innerText<2){
+      btn.innerText = 'ADD TO CART';
+      pbtn.style.display = 'none';
+      mbtn.style.display = 'none';
+     
+ }else{
+      btn.innerText = btn.innerText -1;
+ }
+     
+})
+pbtn.addEventListener("click",()=>{
+     btn.innerText = +(btn.innerText) + 1;
+     if(btn.innerText == 5){
+          pbtn.style.display = 'none';
+          
+     }
+     
+})
+
+}
+
+
+
+
+{
+let btn = document.querySelector('.product-10btn');
+let pbtn = document.querySelector('.buttonn10');
+let mbtn = document.querySelector('.button10');    
+
+btn.addEventListener("click",()=>{
+     btn.innerText = '1';
+     pbtn.style.display = 'inline-block';
+     mbtn.style.display = 'inline-block';
+     
+})
+mbtn.addEventListener("click",()=>{
+ if(btn.innerText<2){
+      btn.innerText = 'ADD TO CART';
+      pbtn.style.display = 'none';
+      mbtn.style.display = 'none';
+     
+ }else{
+      btn.innerText = btn.innerText -1;
+ }
+     
+})
+pbtn.addEventListener("click",()=>{
+     btn.innerText = +(btn.innerText) + 1;
+     if(btn.innerText == 5){
+          pbtn.style.display = 'none';
+          
+     }
+     
+})
+
+}
+
+
+{
+let btn = document.querySelector('.product-11btn');
+let pbtn = document.querySelector('.buttonn11');
+let mbtn = document.querySelector('.button11');    
+
+btn.addEventListener("click",()=>{
+     btn.innerText = '1';
+     pbtn.style.display = 'inline-block';
+     mbtn.style.display = 'inline-block';
+     
+})
+mbtn.addEventListener("click",()=>{
+ if(btn.innerText<2){
+      btn.innerText = 'ADD TO CART';
+      pbtn.style.display = 'none';
+      mbtn.style.display = 'none';
+     
+ }else{
+      btn.innerText = btn.innerText -1;
+ }
+     
+})
+pbtn.addEventListener("click",()=>{
+     btn.innerText = +(btn.innerText) + 1;
+     if(btn.innerText == 5){
+          pbtn.style.display = 'none';
+          
+     }
+     
+})
+
+}
+
+{
+     let btn = document.querySelector('.product-12btn');
+let pbtn = document.querySelector('.buttonn12');
+let mbtn = document.querySelector('.button12');    
+
+btn.addEventListener("click",()=>{
+     btn.innerText = '1';
+     pbtn.style.display = 'inline-block';
+     mbtn.style.display = 'inline-block';
+     
+})
+mbtn.addEventListener("click",()=>{
+ if(btn.innerText<2){
+      btn.innerText = 'ADD TO CART';
+      pbtn.style.display = 'none';
+      mbtn.style.display = 'none';
+     
+ }else{
+      btn.innerText = btn.innerText -1;
+ }
+     
+})
+pbtn.addEventListener("click",()=>{
+     btn.innerText = +(btn.innerText) + 1;
+     if(btn.innerText == 5){
+          pbtn.style.display = 'none';
+          
+     }
+     
+})
+
+}
